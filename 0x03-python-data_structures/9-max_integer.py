57762dbd8c189577f0320027edfbb598d460fd9b
Adetops <heritagediwamu@gmail.com>
2022-12-06 13:57:03 -0800

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,5 +8,5 @@ def max_integer(my_list=[]):
                 temp = my_list[i]
                 my_list[i] = my_list[i + 1]
                 my_list[i + 1] = temp
-            return my_list[0]
+                return my_list[0]
 
