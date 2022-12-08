d4e93987e03bc44eb7292a29799949c95317d22e
Adetops <heritagediwamu@gmail.com>
2022-12-07 19:36:41 -0800

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    my_matrix = [[row[i]**2 for i in range(3)] for row in matrix]
+    my_matrix = [[col[i]**2 for i in range(3)] for col in matrix]
     return my_matrix
